853ed711a85142aeced2b2546e0067e33b03899a
ArsenGabrielyan <arsentech5002@gmail.com>
2022-07-06 12:26:47 +0400

    Added files

---
+++ 1/script.js
@@ -0,0 +1,12 @@
+function countTrue(arr){
+     let count = 0
+     arr.forEach(element => {
+         if(element == true){
+               count++
+         }
+     });
+     console.log(count)
+}
+countTrue([true, false, false, true, false])
+countTrue([false, false, false, false])
+countTrue([]) 
