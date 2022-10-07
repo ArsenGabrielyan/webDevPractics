74180f0e33846372e70c3833f0cc43547e07921c
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2022-10-07 16:52:41 +0400

    Added One More

---
+++ 4/script.js
@@ -0,0 +1,22 @@
+function findMissingValues(arr = []){
+     if(!Array.isArray(arr)){
+          return "This is Not an Array, Try typing an Array like This: [1,2,3,4,5,6,7,8,9]"
+     }
+     let a = arr[1] - arr[0]
+     let result = 0;
+     for(let i=2; i<arr.length; i++){
+          let b = arr[i] - arr[i-1];
+          if(b<a){
+               result = arr[1] - (a/2)
+          } else if(b>a){
+               result = arr[i] - (b/2)
+          }
+     }
+     return result
+}
+
+console.log(findMissingValues([10,20,30,40,60,70,80]))
+console.log(findMissingValues([2,3,8,10,12,14,16]))
+console.log(findMissingValues([5,10,15,20,30,35,40,45,50]))
+console.log(findMissingValues([0,0.5,1,2,2.5,3]))
+console.log(findMissingValues([0,4,6,8,10,12]))
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# JS Test 
-This is a Test
+# JS Exercise Test 
+
+This is a List of Exercise I Did in JS.
+
+- Count How Many Trues in Array Has
+- Find String, That Contains Numbers in Array
+- Count How many Values are in Nested Array in Object
+- Find Missing Number in Array
